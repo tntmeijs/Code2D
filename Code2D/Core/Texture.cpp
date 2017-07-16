533608223b58ebf31998cf839df0f03324cef1ab
tntmeijs <tntmeijs@gmail.com>
2017-07-16 21:41:22 +0100

    Basic texture creation
    The texture class is now able to create a simple 2D texture.

--- Code2D/Core/Texture.cpp
+++ Code2D/Core/Texture.cpp
@@ -1,5 +1,72 @@
 #include "Texture.hpp"
+#include "stb_image.h"
 
 namespace Code2D
 {
+	Texture::Texture()
+	{
+		TextureCoordinates = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+	}
+
+	void Texture::Create(const char * PathToImage)
+	{
+		glGenTextures(1, &TextureID);
+		glBindTexture(GL_TEXTURE_2D, TextureID);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		// Want the retro pixel look for sprites
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// Loading the image data
+		int ImageWidth, ImageHeight, ImageColorChannels;
+		unsigned char * ImageByteData = stbi_load(	PathToImage,
+													&ImageWidth,
+													&ImageHeight,
+													&ImageColorChannels,
+													STBI_rgb_alpha);
+
+		if (ImageColorChannels == STBI_rgb_alpha)
+		{
+			// Use the alpha channel (RGBA)
+			glTexImage2D(	GL_TEXTURE_2D,
+							0,
+							GL_RGBA,
+							ImageWidth,
+							ImageHeight,
+							0,
+							GL_RGBA,
+							GL_UNSIGNED_BYTE,
+							ImageByteData);
+		}
+		else
+		{
+			// No alpha channel available, only RGB
+			glTexImage2D(	GL_TEXTURE_2D,
+							0,
+							GL_RGB,
+							ImageWidth,
+							ImageHeight,
+							0,
+							ImageColorChannels,
+							GL_UNSIGNED_BYTE,
+							ImageByteData);
+		}
+		
+
+		// Image data is no longer needed, because we have already created a texture!
+		stbi_image_free(ImageByteData);
+
+		// Unbind because we are done using this buffer
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	GLuint Texture::Get()
+	{
+		return TextureID;
+	}
 }
--- Code2D/Core/Texture.hpp
+++ Code2D/Core/Texture.hpp
@@ -2,12 +2,21 @@
 #define TEXTURE_HPP
 
 #include "glad/glad.h"
+#include "glm/vec4.hpp"
 
 namespace Code2D
 {
 	class Texture
 	{
 	public:
+		Texture();
+		void Create(const char * PathToImage);
+
+		// Returns the texture GLuint
+		GLuint Get();
+
+		// (uMin, vMin, uMax, vMax)
+		glm::vec4 TextureCoordinates;
 
 	private:
 		GLuint TextureID;
